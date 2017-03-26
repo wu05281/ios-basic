b3fb1261a69d119b97440ef43a9cd193e28116ee
wu05281 <37809131@qq.com>
2017-03-26 16:07:01 +0800

    Initial Commit

---
+++ ios_basic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_basic
+//
+//  Created by wubo on 2017/3/26.
+//  Copyright © 2017年 wubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_basic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_basic
+//
+//  Created by wubo on 2017/3/26.
+//  Copyright © 2017年 wubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
